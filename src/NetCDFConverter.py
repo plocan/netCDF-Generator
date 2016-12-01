8251aa58430fc3ec5096b841c22088369946606e
Juan Carlos Arroyo Herrera <juan.arroyo.ext@plocan.eu>
2016-12-01 10:06:36 +0000

    Update NetCDFConverter.py

--- src/NetCDFConverter.py
+++ src/NetCDFConverter.py
@@ -1,22 +1,23 @@
 from netCDF4 import Dataset
 
+from src.inputData.Data import *
 from src.inputData.Metadata import *
 
 
 class NetCDFConverter(object):
     def __init__(self, metadataFile, csvFile, ncOutput):
-        """self.metadataFile = metadataFile
-        Metadata(metadataFile)
-        self.csvFile = csvFile
-        self.ncOutput = ncOutput
-        self.metadata = Metadata(self.metadataFile)
-        self.data = Data(csvFile)"""
-
-        self.ncOutput = Dataset(ncOutput, 'w', 'NETCDF4')
+        # self.metadataFile = metadataFile
+        # self.csvFile = csvFile
+        # self.ncOutput = ncOutput
+        self.metadata = Metadata(metadataFile)
+        self.data = Data(csvFile)
+        self.ncOutput = Dataset(self.ncOutput, 'w', 'NETCDF4')
         self.globalAttributes = Metadata(metadataFile).getGlobalAttributes()
         self.globalAttributes.writeAttributes(self.ncOutput)
+        self.dimensions.writeDimensions(ncOutput)
 
-
+"""
 if __name__ == '__main__':
     NetCDFConverter('/Users/Loedded/Downloads/xx.json', '/Users/Loedded/Downloads/xx.dat',
                     '/Users/Loedded/Downloads/xx.nc')
+"""

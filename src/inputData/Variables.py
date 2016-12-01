a10c653404fb1816febaaff02c5c1c01b54e934f
Juan Carlos Arroyo Herrera <juan.arroyo.ext@plocan.eu>
2016-12-01 10:16:16 +0000

    Update Variables.py

--- src/inputData/Variables.py
+++ src/inputData/Variables.py
@@ -9,3 +9,20 @@ class Variables():
 
     def getVariableValueIterator(self, key):
         return self.variablesList[key].iteritems()
+        
+    def writeVariables(self, ncFile):
+        variables = self.data['variables']
+        varNames = ['_FillValue', 'variable_name', 'typeof', 'dim', 'value', 'csvcolumn']
+        for var in variables:
+            NcVar = self.createVariables(ncFile, var)
+            #self.add_data(NcVar, var)
+            #self.deleteatts(varNames, var)
+            #self.add_atts(NcVar, var)
+
+    def createVariables(self, ncFile, variable):
+        fillVal = variable['_FillValue'] if '_FillValue' in variable and variable['_FillValue'] != "" else False
+        if 'dim' in variable and variable['dim'] != "":
+           ncVar = ncFile.createVariable(variable['variable_name'], variable['typeof'], (variable['dim']), fill_value=fillVal, zlib=True,complevel=9)
+        else:
+           ncVar = ncFile.createVariable(variable['variable_name'], variable['typeof'], fill_value=fillVal, zlib=True, complevel=9)
+        return ncVar

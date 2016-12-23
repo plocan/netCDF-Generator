67e17465eef348b814d7190fddc9cbe319823405
Pablo Armas Martín <pablo.armas.ext@plocan.eu>
2016-12-23 13:28:22 +0000

    Diccionario añadido y su lectura.

--- src/NetCDFConverter.py
+++ src/NetCDFConverter.py
@@ -7,13 +7,13 @@ from Log import Log
 from Metadata import *
 from Writer import Writer
 from Checker import Checker
+from Reader_dictionary import Reader_dictionary
 
 
 class NetCDFConverter(object):
     def __init__(self, metadataFile, csvFile, ncOutput):
         Log().set_log_info("[Begin] conversion to NetCDF of: " + metadataFile + "  " + csvFile + "  " + ncOutput)
-        self.init_elements(metadataFile, csvFile, ncOutput)
-
+        self.dimensions_dictionary = Reader_dictionary().get_dimensions_dictionary()
         if not os.path.exists(self.ncOutput):
             try:
                 self.ncFile = Dataset(self.ncOutput, 'w', format='NETCDF' + self.version)
@@ -23,12 +23,9 @@ class NetCDFConverter(object):
                 self.version = '4_CLASSIC'
                 self.ncFile = Dataset(self.ncOutput, 'w', format='NETCDF' + self.version)
             self.create_netcdf()
-
         else:  # Append start
             self.ncFile = Dataset(self.ncOutput, 'r+')
             self.append_netcdf()
-
-
         self.metadata.globalAttributes.max_min_attribute(self.ncFile)
         self.ncFile.close()
         Log().set_log_info("[Finished] conversion to NetCDF of : " + metadataFile + "  " + csvFile + "  " + ncOutput)
@@ -44,6 +41,9 @@ class NetCDFConverter(object):
         self.temporalAppendPosition = {}
         self.dimensions = self.metadata.get_dimensions()
         self.globalAttributes = Metadata(metadataFile).get_global_attributes()
+        naming_authority = self.globalAttributes.attributesList['naming_authority']
+        if naming_authority == 'EGO':
+            self.dimensions_dictionary = Reader_dictionary().get_dimensions_dictionary()
 
     def create_netcdf(self):
         self.globalAttributes.write_attributes(self.ncFile)
@@ -59,7 +59,7 @@ class NetCDFConverter(object):
 
 
 if __name__ == '__main__':
-    NetCDFConverter('/Users/Loedded/Downloads/xx.json', '/Users/Loedded/Downloads/xx.dat', '/Users/Loedded/Downloads')
+    NetCDFConverter('/Users/Loedded/Downloads/YY.json', '/Users/Loedded/Downloads/YY.dat', '/Users/Loedded/Downloads')
 
 """
 if __name__ == '__main__':
